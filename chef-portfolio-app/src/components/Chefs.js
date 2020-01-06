89f1778b8d98e0b000cf37b9a5e78bcd9ecd4bff
brittanymae01 <brittanymae01>
2020-01-06 13:59:51 -0800

    connect mapStateToProps to Chefs and SignUp

--- chef-portfolio-app/src/components/Chefs.js
+++ chef-portfolio-app/src/components/Chefs.js
@@ -1,1 +1,16 @@
 //map over the array of chefs and render a ChefCard for each chef here
+import React from "react";
+import { connect } from "react-redux";
+
+const Chefs = props => {
+  return <div></div>;
+};
+
+const mapStateToProps = state => {
+  return {
+    chefs: state.chefs,
+    error: state.error
+  };
+};
+console.log(chefs);
+export default connect(mapStateToProps, {})(Chefs);
