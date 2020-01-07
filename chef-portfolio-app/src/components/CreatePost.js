830aae5c4de833768303a6f129d5cba16696cb9b
Zachary Mayfield <zakmayfield@gmail.com>
2020-01-07 14:00:54 -0500

    created a CreatePost component that takes in data, also with axios post request / haven't tested this out to make sure it works completely / Not Styled

--- chef-portfolio-app/src/components/CreatePost.js
+++ chef-portfolio-app/src/components/CreatePost.js
@@ -2,10 +2,14 @@
 // use axios.post request on form submit
 // route back to chef profile page on submit
 
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import Page from "../pages/Page";
 import { connect } from "react-redux";
 import { postNewRecipe } from "../actions/index";
+import {Formik, Form, Field } from 'formik'
+import axios from 'axios';
+import * as Yup from 'yup'
 
 function CreatePost(props) {
   //this is only for demonstration purposes- include axios post with this props.history.push
@@ -13,10 +17,88 @@ function CreatePost(props) {
     props.history.push("/chef-profile/:id");
   };
 
+  const [recipes, setRecipes] = useState([]);
+
+  const userSchema = Yup.object().shape({
+    title: Yup.string().required(),
+    time: Yup.string().required(),
+    ingredients: Yup.string().required(),
+    instructions: Yup.string().required()
+  });
+
   return (
     <>
       <Page>
-        <h1>I'll be a new post form when I'm all grown</h1>
+        <h1>Create Post</h1>
+
+        <Formik 
+          initialValues={{
+            title: "",
+            time: "",
+            ingredients: "",
+            instructions: ""
+          }}
+          onSubmit={(values, tools) => {
+            tools.resetForm();
+
+            axios.post('https://reqres.in/api/users', values)
+              .then(res => {
+                console.log('SUCCESSFUL', res)
+
+                const recipeData = res.data;
+
+                setRecipes([...recipes, recipeData])
+              })
+              .catch(err => {
+                console.log('FAILURE', err)
+              })
+          }}
+          validationSchema={userSchema}
+          render={props => {
+            return(
+              <Form className="formContainer">
+                <label>
+                  Title: 
+                  <Field  
+                    name="title" 
+                    type="text"
+                    placeholder="Squash and Feta Flatbread"
+                  />
+                </label>
+
+                <label>
+                  Time to complete:
+                  <Field 
+                    name="time" 
+                    type="text"
+                    placeholder="Approximately 1 hour"  
+                  />
+                </label>
+
+                <label>
+                  Ingredients:
+                  <Field
+                    name="ingredients"
+                    as="textarea"
+                    placeholder="List of ingredients needed"
+                  />
+                </label>
+
+                <label>
+                  Instructions:
+                  <Field
+                    name="instructions"
+                    as="textarea"
+                    placeholder="List of instructions"
+                  />
+                </label>
+
+                <input type="submit"/>
+              </Form>
+            )
+          }}
+        />
+
         <button onClick={routeToChefProfile}>Save</button>
       </Page>
     </>
